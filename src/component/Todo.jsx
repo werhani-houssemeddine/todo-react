95ad4c7b21d4d3cb893e4bbbef9501067f7f51db
Houssemeddine Werhani <houssemeddinewerhani@gmail.com>
2022-10-24 06:28:47 +0100

    I write function to handle check, edit and delete event for the moment only check works fine

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,14 +1,19 @@
 import './stylesheet/todo.css';
 import { FaTrash, FaEdit } from 'react-icons/fa';
 
-function Todo({ content }) {
+function Todo({ content, methods }) {
+  const { id, todo, isDone } = content;
+  const { editHandler, delHandler, checkHandler } = methods;
+
+  //console.log({ methods });
+
   return (
     <div className="todo-container">
-      <input type="checkbox" name="" id="" />
-      <span htmlFor="">{content}</span>
+      <input type="checkbox" name="" checked={isDone} onChange={() => checkHandler(id)} />
+      <span htmlFor="">{todo}</span>
       <div className="icons">
-        <FaEdit color="white" />
-        <FaTrash color="white" />
+        <FaEdit color="white" onClick={() => editHandler(id)} />
+        <FaTrash color="white" onClick={() => delHandler(id)} />
       </div>
     </div>
   );
--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from 'react';
 import Todo from './Todo';
 
 function TodoList() {
+  const [todo, setTodo] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:1025/todo-api')
+      .then((res) => res.json())
+      .then(({ data }) => setTodo(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  //console.log(todo);
+
+  const fns = () => ({
+    editHandler: (id) => {
+      console.log('Edit handler fn clicked', id);
+    },
+    delHandler: (id) => {
+      console.log('Delete handler fn clicked', id);
+    },
+
+    // To handler the checkbox first we search for the todo with the help of map method
+    // and we make a test if the element id is the same we make the change otherwise we 
+    // return the same element without any change
+    checkHandler: (id) => {
+      setTodo((prev) =>
+        prev.map((element) => {
+          return element.id != id
+            ? element
+            : { ...element, isDone: !element.isDone };
+        })
+      );
+    },
+  });
+
   return (
     <>
-      <Todo content="Hello friends" />
-      <Todo content="Hello friends" />
-      <Todo content="Hello friends" />
+      {todo.length === 0 ? (
+        <h1>No Element ...</h1>
+      ) : (
+        todo.map((ele) => <Todo content={ele} key={ele.id} methods={fns()} />)
+      )}
     </>
   );
 }
